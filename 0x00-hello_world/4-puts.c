e1901c1fcb8400f464d5607261396a7134c21874
22Benji <binibtc@gmail.com>
2023-06-15 11:28:34 -0700

    cprogram1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 /*
  * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
+ * C program that prints exactly "Programming is like building a multilingual puzzle
  */
 int main(void)
 {
