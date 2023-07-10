b35070db38c0de1937a3c97a6e6d25b1be168eb2
22Benji <binibtc@gmail.com>
2023-07-10 11:43:03 -0700

    malloc updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,8 +9,7 @@
  * Return: pointer to array, NULL if fail
  */
 
-char *create_array(unsigned int size, char c)
-
+char *create_array(unsigned int size, char c):
 {
 	char *str;
 	unsigned int i;
@@ -18,7 +17,6 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)  
 		str[i] = c;
 	return (str);
