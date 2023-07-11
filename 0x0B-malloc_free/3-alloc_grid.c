fb72f24dc9ef30e6a661675b482b12525f142b1f
22Benji <binibtc@gmail.com>
2023-07-11 12:34:26 -0700

    malloc update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 
-/***
+/**
  * alloc_grid - nested loop to make grid
  * @width: width input
  * @height: height input
@@ -11,7 +11,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **mee;
-        int x, y;
+	int x, y;
         
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -25,18 +25,44 @@ int **alloc_grid(int width, int height)
 		{
 			for (; x >= 0; x--)
 				free(mee[x]);
-                        free(mee);
-	                return (NULL);
-                }
-        }
-        for (x = 0; x < height; x++)
-        {
-                for (y = 0; y < width; y++)
-	                mee[x][y] = 0;
-        }
-        return (mee);
-
+			free(mee);
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+	return (mee);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
